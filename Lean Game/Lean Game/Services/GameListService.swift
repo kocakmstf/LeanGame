91de99386adac185dc41486ff23c3cfb2b040500
Mustafa KOCAK <mkocak@sanko.com.tr>
2020-08-11 12:50:57 +0300

    view models namespaces preperation

---
+++ Lean Game/Lean Game/Services/GameListService.swift
@@ -0,0 +1,12 @@
+//
+//  GameListService.swift
+//  Lean Game
+//
+//  Created by Mustafa KOCAK on 10.08.2020.
+//  Copyright © 2020 Mustafa Kocak. All rights reserved.
+//
+
+import Foundation
+import LeanContract
+import LeanApi
+
